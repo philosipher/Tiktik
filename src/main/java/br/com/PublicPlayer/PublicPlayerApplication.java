9197b3c4b680d858322b659f7993b8b69c835b01
Diogo Vilela <90631238+DiogoVilela12@users.noreply.github.com>
2023-07-11 18:03:12 -0300

    Commit Inicial

---
+++ src/main/java/br/com/PublicPlayer/PublicPlayerApplication.java
@@ -0,0 +1,13 @@
+package br.com.PublicPlayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicPlayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicPlayerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/PublicPlayer/PublicPlayerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.PublicPlayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicPlayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
